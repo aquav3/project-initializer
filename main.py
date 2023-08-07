9d4798cb73581d1d91f26a7426bca9937114be98
aquav3 <kyrbynyt@gmail.com>
2023-08-07 14:12:59 +0300

    Starting python work

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello, world!")
+print("Welcome to project-initializer")
+
+print("Enter project name: ", end="")
+projName = input()
+
+print("1: python")
+projType = int(input("Enter option: "))
+
+if projType == 1:
+    print("Picked python!")
+
